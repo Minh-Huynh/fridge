709dcd656bd9a97c58bf82f188028da26cd83866
Minh Huynh <minhh1990@gmail.com>
2022-03-05 13:48:10 -0800

    add fridge interface functions

--- src/fridge.erl
+++ src/fridge.erl
@@ -3,29 +3,58 @@
 -record(state, {inside}).
 
 start() ->
-    spawn(?MODULE, init, []).
+    %we register the connection to the fridge event server
+    %so we can access using ?MODULE. 
+    %Variables starting with question marks generally are macro expansions
+    %but in this case, you can also just use as a regular atom/variable
+    register(?MODULE, Pid=spawn(?MODULE, init, [])),
+    Pid.
 
 init() ->
     loop(#state{inside=maps:new()}).
+
+put_in(Name, Seconds) ->
+    Ref = make_ref(),
+    ?MODULE ! {put_in, self(), Ref, Name, Seconds},
+    receive
+        {Ref, ok} ->
+            ok
+    after 5000 ->
+        {error,timeout}
+    end.
+
+take_out(Name) ->
+    Ref = make_ref(),
+    ?MODULE ! {take_out, self(), Ref, Name},
+    receive
+        {Ref, ok} ->
+            ok
+    after 5000 ->
+        {error,timeout}
+    end.
+
+list_inventory() ->
+    ?MODULE ! {list_food}.
+
 loop(State) ->
     receive
-        {put_in, Name, SecondsToExpiration} ->
+        {put_in, Pid, MsgRef, Name, SecondsToExpiration} ->
             Ref = food:start(Name, SecondsToExpiration),
             NewInside = maps:put(Name, Ref, State#state.inside),
             io:format("Food put in: ~p~n", [Name]),
             NewState = #state{inside=NewInside},
+            Pid ! {MsgRef, ok},
             loop(NewState);
-        {take_out, Name} ->
-            Pid = maps:get(Name, State#state.inside),
+        {take_out, Pid, MsgRef, Name} ->
             Ref = erlang:monitor(process, Pid),
             Pid ! {self(), Ref, cancel},
             NewInside = maps:remove(Name, State#state.inside),
             io:format("Food taken out: ~p~n", [Name]),
             NewState = #state{inside=NewInside},
+            Pid ! {MsgRef, ok},
             loop(NewState);
         {list_food} -> 
             print_list(maps:keys(State#state.inside)),
-            io:format(State#state.inside),
             loop(State);
         {done, Name} ->
             NewInside = maps:remove(Name, State#state.inside),
